7fa78874621c8ae7f8fd7be8ff3d0101f1e44381
songshaoyu <571398414@qq.com>
2016-11-25 20:20:23 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -35,10 +35,35 @@ class GroupsController < ApplicationController
     	@group = Group.new(group_params)
     	@group.user = current_user
     	if @group.save
+    		current_user.join!(@group)
     	   redirect_to groups_path
         else
     	   render :new
         end
+    end
+
+    def join
+    	@group = Group.find(params[:id])
+
+    	if !current_user.is_member_of?(@group)
+    		current_user.join!(group)
+    		flash[:notice] = "加入本讨论版成功"
+    	else
+    		flsh[:warning] = "你已经是本讨论版成员了！"
+    		
+    	end
+    	redirect_to group_path(@group)
+    end
+
+    def quit
+    	@group = Group.find(params[:id])
+    	if current_user.is_member_of?(@group)
+    		current_user.quit!(@group)
+    		flash[:alert] = "已经退出本讨论版"
+    	else
+    		flash[:warning] = "你不是本讨论版成员，怎么退出少羽"
+    	end
+    	redirect_to group_path(@group)
     end
 
     private
